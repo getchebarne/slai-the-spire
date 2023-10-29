293f72174f97599731b7e7c85c50fb3af59fdc4e
Guillermo Etchebarne <getchebarne@tryolabs.com>
2023-10-29 18:18:21 -0300

    Scrap initial ECS design

--- src/game/components/effect.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-from components.base import BaseComponent
-
-
-@dataclass
-class Effect(BaseComponent):
-    name: str
-    value: int
--- src/game/components/health.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-from components.base import BaseComponent
-
-
-@dataclass
-class Health(BaseComponent):
-    current: int
-    max: int
