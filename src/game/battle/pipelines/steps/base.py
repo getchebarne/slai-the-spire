0fdabc692e447e39ae00b0d8c21cbc85c3e46787
Guillermo Etchebarne <getchebarne@tryolabs.com>
2023-11-05 14:46:56 -0300

    Add pipeline step priority

--- src/game/battle/pipelines/steps/base.py
+++ src/game/battle/pipelines/steps/base.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
 
 from game.effects.base import BaseEffect
@@ -8,11 +9,23 @@ class BaseStep(ABC):
         if self._condition(effect):
             self._apply_effect(effect)
 
+    @property
+    @abstractmethod
+    def priority(self) -> int:
+        raise NotImplementedError
+
     @abstractmethod
     def _apply_effect(self, effect: BaseEffect) -> None:
         raise NotImplementedError
 
-    # TODO: should `target` also be an argument here?
     @abstractmethod
     def _condition(self, effect: BaseEffect) -> bool:
         raise NotImplementedError
+
+    def __lt__(self, other: BaseStep) -> bool:
+        if not isinstance(other, BaseStep):
+            raise TypeError(
+                f"'<' not supported between instances of '{type(self)}' and '{type(other)}'"
+            )
+
+        return self.priority < other.priority
