8a187b2db9b1657ff30a721ed3d5695343085ebc
Guillermo Etchebarne <getchebarne@tryolabs.com>
2023-11-07 18:28:26 -0300

    Move EffectPipeline to BattleEngine and improve relic registration

--- src/game/entities/actors/monsters/group.py
+++ src/game/entities/actors/monsters/group.py
@@ -1,4 +1,4 @@
-from typing import Iterable
+from typing import Iterable, Iterator
 
 from game.entities.actors.monsters.base import Monster
 
@@ -6,7 +6,7 @@ from game.entities.actors.monsters.base import Monster
 class MonsterGroup(list):
     def __init__(self, iterable: Iterable[Monster]):
         if not all(isinstance(item, Monster) for item in iterable):
-            raise ValueError("All elements must be Monsters")
+            raise ValueError(f"All elements must be instances of {Monster}")
 
         super().__init__(iterable)
 
@@ -21,6 +21,12 @@ class MonsterGroup(list):
 
     def __add__(self) -> None:
         raise NotImplementedError
+
+    def __getitem__(self, idx: int) -> Monster:
+        return super().__getitem__(idx)
+
+    def __iter__(self) -> Iterator[Monster]:
+        return super().__iter__()
 
     def __str__(self) -> str:
         return "\n".join([f"{idx}) {monster}" for idx, monster in enumerate(self)])
