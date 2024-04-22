c9c88487cf855bfdc581a52a7d1db5ffc8c8c5ad
Guillermo Etchebarne <guillermo.etchebarne@gmail.com>
2024-04-22 12:29:58 -0300

    Minor tests aesthetic changes

--- tests/game/logic/card/test_neutralize.py
+++ tests/game/logic/card/test_neutralize.py
@@ -6,13 +6,16 @@ from src.game.logic.card.neutralize import DAMAGE
 from src.game.logic.card.neutralize import WEAK
 
 
+MONSTER_ENTITY_ID = 1
+
+
 def test_base():
     # Instantiate the Agent, Context, and Engine
     agent = RandomAgent()
     context = Context(
         entities={
             0: EntityData(name="Silent", max_health=50),
-            1: EntityData(name="Dummy", max_health=50),
+            MONSTER_ENTITY_ID: EntityData(name="Dummy", max_health=50),
         },
         hand=["Neutralize"],
         active_card_idx=0,
@@ -21,12 +24,12 @@ def test_base():
 
     # Store previous health & weak
     prev_health = context.entities[context.CHAR_ENTITY_ID].current_health
-    prev_weak = context.entity_modifiers[(1, "Weak")]
+    prev_weak = context.entity_modifiers[(MONSTER_ENTITY_ID, "Weak")]
 
     # Play the card
-    engine._play_card(monster_entity_id=1)
+    engine._play_card(MONSTER_ENTITY_ID)
 
     # Assert that the monster's health has decreased by DAMAGE and that the monster's "Weak"
     # modifier has increased by WEAK
-    assert context.entities[1].current_health == prev_health - DAMAGE
-    assert context.entity_modifiers[(1, "Weak")] == prev_weak + WEAK
+    assert context.entities[MONSTER_ENTITY_ID].current_health == prev_health - DAMAGE
+    assert context.entity_modifiers[(MONSTER_ENTITY_ID, "Weak")] == prev_weak + WEAK
