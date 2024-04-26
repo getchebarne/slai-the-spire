33932c607b02ab5d981b25720a1cd83fa4210d00
Guillermo Etchebarne <guillermo.etchebarne@gmail.com>
2024-04-25 22:30:59 -0300

    Add monster move tests

--- src/game/logic/move/defend.py
+++ src/game/logic/move/defend.py
@@ -4,7 +4,10 @@ from src.game.core.effect import EffectType
 from src.game.logic.move.base import BaseMoveLogic
 
 
+BLOCK = 6
+
+
 class DefendLogic(BaseMoveLogic):
     # TODO: improve owner def
     def use(self, context: Context, source_entity_id: int) -> list[Effect]:
-        return [Effect(source_entity_id, source_entity_id, EffectType.BLOCK, 6)]
+        return [Effect(source_entity_id, source_entity_id, EffectType.BLOCK, BLOCK)]
--- tests/game/logic/move/test_attack.py
+++ tests/game/logic/move/test_attack.py
@@ -2,7 +2,7 @@ from src.agents.random import RandomAgent
 from src.game.battle.engine import BattleEngine
 from src.game.context import Context
 from src.game.context import EntityData
-from src.game.logic.card.strike import DAMAGE
+from src.game.logic.move.attack import DAMAGE
 
 
 MONSTER_ENTITY_ID = 1
@@ -16,16 +16,15 @@ def test_base():
             Context.CHAR_ENTITY_ID: EntityData(name="Silent", max_health=50),
             MONSTER_ENTITY_ID: EntityData(name="Dummy", max_health=50),
         },
-        hand=["Strike"],
-        active_card_idx=0,
+        monster_moves={MONSTER_ENTITY_ID: "Attack"},
     )
     engine = BattleEngine(agent, context)
 
     # Store previous health
-    prev_health = context.entities[MONSTER_ENTITY_ID].current_health
+    prev_health = context.entities[context.CHAR_ENTITY_ID].current_health
 
-    # Play the card
-    engine._play_card(MONSTER_ENTITY_ID)
+    # Execute the move
+    engine._execute_monster_move(MONSTER_ENTITY_ID)
 
-    # Assert that the monster's health has decreased by DAMAGE
-    assert context.entities[MONSTER_ENTITY_ID].current_health == prev_health - DAMAGE
+    # Assert that the character's health has decreased by DAMAGE
+    assert context.entities[context.CHAR_ENTITY_ID].current_health == prev_health - DAMAGE
