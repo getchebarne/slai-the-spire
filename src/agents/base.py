9991bcc716948c296a6a0700352b18c9bfd57be1
Guillermo Etchebarne <guillermo.etchebarne@gmail.com>
2024-05-04 14:20:24 -0300

    Add missing argument in BaseAgent's select_action

--- src/agents/base.py
+++ src/agents/base.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
 
 from src.game.battle.engine import Action
+from src.game.context import Context
 
 
 class BaseAgent(ABC):
     @abstractmethod
-    def select_action(self) -> Action:
+    def select_action(self, context: Context) -> Action:
         raise NotImplementedError
