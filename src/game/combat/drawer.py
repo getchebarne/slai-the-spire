64b80c9c4fd60a49c59910b3c59b2150228932f8
Guillermo Etchebarne <guillermo.etchebarne@gmail.com>
2024-06-11 22:57:30 -0300

    Improve drawing module

--- src/game/combat/drawer.py
+++ src/game/combat/drawer.py
@@ -1,4 +1,5 @@
 import os
+from typing import Optional
 
 from src.game.combat.view import CombatView
 from src.game.combat.view import Energy
@@ -13,7 +14,7 @@ N_TERM_COLS, _ = os.get_terminal_size()
 
 
 def _energy_str(energy: Energy) -> str:
-    return f"{energy.current} / {energy.max}"
+    return f"\U0001F50B {energy.current}/{energy.max}"
 
 
 def _card_str(card: Card) -> str:
@@ -22,38 +23,63 @@ def _card_str(card: Card) -> str:
 
 def _hand_str(hand: list[Card]) -> str:
     # TODO: If there's an active card, return its name in green
-    str_ = " / ".join([_card_str(card_name) for i, card_name in enumerate(hand)])
-    str_ = f"[ {str_} ]"
-    return str_
+    return " / ".join(
+        [
+            (f"\033[92m{_card_str(card)}\033[0m" if card.is_selected else _card_str(card))
+            for card in hand
+        ]
+    )
 
 
 def _health_str(health: Health) -> str:
-    return f"{health.current} / {health.max}"
+    return f"\U0001FAC0 {health.current} / {health.max}"
 
 
 def _block_str(block: Block) -> str:
-    return f"{block.current} / {block.max}"
+    return f"\U0001F6E1 {block.current}"
 
 
 def _creature_str(creature: Character | Monster) -> str:
     return f"{creature.name} {_health_str(creature.health)} {_block_str(creature.block)}"
 
 
-def draw_view(view: CombatView) -> None:
-    # Energy
-    print(_energy_str(view.energy))
-
+def _view_str(view: CombatView) -> str:
     # Monsters
-    for monster in view.monsters:
-        # Print to the right side of the terminal
-        print(f"{_creature_str(monster):>{N_TERM_COLS}}")
+    monster_strs = [_creature_str(monster) for monster in view.monsters]
+    right_justified_monsters = "\n".join(
+        [f"{monster_str:>{N_TERM_COLS}}" for monster_str in monster_strs]
+    )
 
     # Character
-    print(_creature_str(view.character))
-    print("\n")
+    character_str = _creature_str(view.character)
 
-    # Hand
-    print(_hand_str(view.hand))
+    # Energy & hand
+    energy_hand_str = f"{_energy_str(view.energy)} | {_hand_str(view.hand)}"
 
     # Separator
-    print("-" * N_TERM_COLS)
+    separator = "-" * N_TERM_COLS
+
+    return (
+        right_justified_monsters
+        + "\n"
+        + character_str
+        + "\n\n"
+        + energy_hand_str
+        + "\n"
+        + separator
+    )
+
+
+class CombatDrawer:
+    def __init__(self, prev_view_str: Optional[str] = None):
+        self.prev_view_str = prev_view_str
+
+    def __call__(self, combat_view: CombatView) -> None:
+        view_str = _view_str(combat_view)
+
+        if view_str != self.prev_view_str:
+            print(view_str)
+            self.prev_view_str = view_str
+
+
+drawer = CombatDrawer()
