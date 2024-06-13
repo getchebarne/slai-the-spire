23948c4943c6c66d95702724fe7eab3df5438e28
Guillermo Etchebarne <guillermo.etchebarne@gmail.com>
2024-06-13 10:35:46 -0300

    Improve HandleInputSystem

--- src/game/ecs/systems/handle_input.py
+++ src/game/ecs/systems/handle_input.py
@@ -12,11 +12,45 @@ from src.game.ecs.manager import ECSManager
 from src.game.ecs.systems.base import BaseSystem
 
 
+def _promote_card_from_selected_to_played(manager: ECSManager) -> None:
+    # Get currently selected card
+    card_entity_id, _ = next(manager.get_components(CardInHandComponent, IsSelectedComponent))
+
+    # Untag card as selected & tag it as played
+    manager.remove_component(card_entity_id, IsSelectedComponent)
+    manager.add_component(card_entity_id, CardIsPlayedComponent())
+
+
+def _handle_select_card(manager: ECSManager, action_target_entity_id: int) -> None:
+    # Deselect current selected entities
+    manager.destroy_component(IsSelectedComponent)
+
+    # Tag card as selected
+    manager.add_component(action_target_entity_id, IsSelectedComponent())
+
+
+def _handle_confirm(manager: ECSManager) -> None:
+    _promote_card_from_selected_to_played(manager)
+
+
+def _handle_select_monster(manager: ECSManager, action_target_entity_id: int) -> None:
+    _promote_card_from_selected_to_played(manager)
+
+    # Tag monster as card's target
+    manager.add_component(action_target_entity_id, CardTargetComponent())
+
+
+def _handle_end_turn(manager: ECSManager) -> None:
+    # Get character
+    character_entity_id, _ = next(manager.get_component(CharacterComponent))
+
+    # Finish its turn and trigger its turn end. TODO: improve comment
+    manager.remove_component(character_entity_id, IsTurnComponent)
+    manager.add_component(character_entity_id, TurnEndComponent())
+
+
 class HandleInputSystem(BaseSystem):
     def process(self, manager: ECSManager) -> None:
-        # TODO: this doesn't go here
-        manager.destroy_component(CardIsPlayedComponent)
-
         if input_.action is None:
             return
 
@@ -33,30 +67,18 @@ class HandleInputSystem(BaseSystem):
         ):
             raise ValueError(f"Entity {action_target_entity_id} cannot be selected")
 
+        # Select card
         if action_type == ActionType.SELECT_CARD:
-            # TODO: check if there's enough energy here
-            manager.add_component(action_target_entity_id, IsSelectedComponent())
+            _handle_select_card(manager, action_target_entity_id)
+
+        # Confirm
+        if action_type == ActionType.CONFIRM:
+            _handle_confirm(manager)
 
         # Select monster
         if action_type == ActionType.SELECT_MONSTER:
-            card_is_selected_entity_id, _ = next(
-                manager.get_components(CardInHandComponent, IsSelectedComponent)
-            )
-            manager.remove_component(card_is_selected_entity_id, IsSelectedComponent)
-            manager.add_component(card_is_selected_entity_id, CardIsPlayedComponent())
+            _handle_select_monster(manager, action_target_entity_id)
 
-            # Select
-            manager.add_component(action_target_entity_id, CardTargetComponent())
-
-        # Confirm. TODO: maybe remove confirm
-        if action_type == ActionType.CONFIRM:
-            card_is_selected_entity_id, _ = next(
-                manager.get_components(CardInHandComponent, IsSelectedComponent)
-            )
-            manager.remove_component(card_is_selected_entity_id, IsSelectedComponent)
-            manager.add_component(card_is_selected_entity_id, CardIsPlayedComponent())
-
+        # End turn
         if action_type == ActionType.END_TURN:
-            character_entity_id, _ = next(manager.get_component(CharacterComponent))
-            manager.remove_component(character_entity_id, IsTurnComponent)
-            manager.add_component(character_entity_id, TurnEndComponent())
+            _handle_end_turn(manager)
