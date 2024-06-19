693614162a331f95aa47b947f0b6b3f045b7765b
Guillermo Etchebarne <guillermo.etchebarne@gmail.com>
2024-06-19 11:29:04 -0300

    Rename EffectIsQueuedComponent's priority to position

--- src/game/ecs/systems/base.py
+++ src/game/ecs/systems/base.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 from src.game.ecs.manager import ECSManager
 
 
-# TODO: add priority
+# TODO: add position
 class BaseSystem(ABC):
     @abstractmethod
     def process(self, manager: ECSManager) -> None:
