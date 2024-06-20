85a377e13344ca2f35f761a348fc5bea43c0febd
Guillermo Etchebarne <guillermo.etchebarne@gmail.com>
2024-06-19 21:49:21 -0300

    Improve terminal drawings

--- src/game/combat/drawer.py
+++ src/game/combat/drawer.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 from src.game.combat.view import CombatView
 from src.game.combat.view.actor import ActorView
+from src.game.combat.view.actor import ModifierView
 from src.game.combat.view.actor import BlockView
 from src.game.combat.view.actor import HealthView
 from src.game.combat.view.card import CardView
@@ -16,7 +17,7 @@ N_TERM_COLS, _ = os.get_terminal_size()
 
 
 def _energy_str(energy: EnergyView) -> str:
-    return f"\U0001F50B {energy.current}/{energy.max}"
+    return f"ENERGY: {energy.current}/{energy.max}"
 
 
 def _card_str(card: CardView) -> str:
@@ -24,7 +25,7 @@ def _card_str(card: CardView) -> str:
 
 
 def _hand_str(hand: list[CardView]) -> str:
-    return " / ".join(
+    return "HAND: " + " / ".join(
         [
             (f"\033[92m{_card_str(card)}\033[0m" if card.is_selected else _card_str(card))
             for card in hand
@@ -33,24 +34,34 @@ def _hand_str(hand: list[CardView]) -> str:
 
 
 def _health_str(health: HealthView) -> str:
-    return f"\U0001FAC0 {health.current} / {health.max}"
+    return f"HP: {health.current}/{health.max}"
 
 
 def _block_str(block: BlockView) -> str:
-    return f"\U0001F6E1 {block.current}"
+    return f"BLK: {block.current}"
 
 
 def _actor_str(actor: ActorView) -> str:
+    modifier_strs = "\n".join([_modifier_str(modifier_view) for modifier_view in actor.modifiers])
     return (
-        f"{actor.name} {_health_str(actor.health)} {_block_str(actor.block)}" f"{actor.modifiers}"
+        f"{actor.name}\n"
+        f"{'-' * len(actor.name)}\n"
+        f"{_health_str(actor.health)}\n"
+        f"{_block_str(actor.block)}\n"
+        f"{modifier_strs}"
     )
+
+
+def _modifier_str(modifier_view: ModifierView) -> str:
+    # TODO: create modifier abbreviations (e.g., "Weak" -> "WK")
+    return f"{modifier_view.type.upper()}: {modifier_view.stacks}"
 
 
 def _effect_str(effect_view: Optional[EffectView]) -> str:
     if effect_view is None:
         return "None"
 
-    return f"{effect_view.type} | {effect_view.number_of_targets}"
+    return f"{effect_view.type}: {effect_view.number_of_targets}"
 
 
 def _intent_str(intent_view: Optional[IntentView]) -> str:
@@ -59,36 +70,56 @@ def _intent_str(intent_view: Optional[IntentView]) -> str:
         return str_
 
     if intent_view.damage is not None:
-        str_ = f"{str_} \U0001F5E1 {intent_view.damage}"
+        str_ = f"{str_}{intent_view.damage}"
 
     if intent_view.times is not None:
         str_ = f"{str_} x {intent_view.times}"
 
     if intent_view.block:
-        str_ = f"{str_} \U0001F6E1"
+        if str_ != "":
+            str_ = f"{str_} & Blocking"
+
+        else:
+            str_ = "Blocking"
 
     return str_
 
 
 def _monster_str(monster_view: MonsterView) -> str:
-    return f"{_intent_str(monster_view.intent)} | {_actor_str(monster_view)}"
+    # Get base actor string
+    str_ = _actor_str(monster_view)
+
+    # Split into lines
+    lines = str_.split("\n")
+
+    # Insert monster's intent at first position
+    lines.insert(0, _intent_str(monster_view.intent))
+
+    # Align lines to the right of the terminal
+    right_aligned_lines = [f"{line:>{N_TERM_COLS}}" for line in lines]
+
+    # Stitch together and return
+    return "\n".join(right_aligned_lines)
 
 
-def _view_str(view: CombatView) -> str:
+def _view_str(view: CombatView) -> str:  #
     # Effect
     effect_str = _effect_str(view.effect)
 
     # Monsters
-    monster_strs = [f"{_monster_str(monster)}" for monster in view.monsters]
-    right_justified_monsters = "\n".join(
-        [f"{monster_str:>{N_TERM_COLS}}" for monster_str in monster_strs]
-    )
+    monster_strs = "\n".join([f"{_monster_str(monster)}" for monster in view.monsters])
+    # right_justified_monsters = "\n".join(
+    #     [f"{monster_str:>{N_TERM_COLS}}" for monster_str in monster_strs]
+    # )
 
     # Character
     character_str = _actor_str(view.character)
 
-    # Energy & hand
-    energy_hand_str = f"{_energy_str(view.energy)} | {_hand_str(view.hand)}"
+    # Energy
+    energy_str = _energy_str(view.energy)
+
+    # Hand
+    hand_str = _hand_str(view.hand)
 
     # Separator
     separator = "-" * N_TERM_COLS
@@ -96,11 +127,13 @@ def _view_str(view: CombatView) -> str:
     return (
         effect_str
         + "\n"
-        + right_justified_monsters
+        + monster_strs
         + "\n"
         + character_str
-        + "\n\n"
-        + energy_hand_str
+        + "\n"
+        + energy_str
+        + "\n"
+        + hand_str
         + "\n"
         + separator
     )
