1e2f2ab63944229fdbd00ba3c46bfa82c7cede74
Guillermo Etchebarne <guillermo.etchebarne@gmail.com>
2024-07-04 19:49:35 -0300

    Improve memory code

--- src/agents/dqn/memory.py
+++ src/agents/dqn/memory.py
@@ -28,58 +28,69 @@ class Sample:
 
 class ReplayBuffer:
     def __init__(self, size: int):
-        self.size = size
+        self._size = size
 
-        self.state_ts: list[CombatView] = [None] * size
-        self.state_tp1s: list[CombatView] = [None] * size
-        self.actions: list[int] = [None] * size
-        self.rewards: list[float] = [None] * size
-        self.valid_action_mask_tp1s: list[CombatView] = [None] * size
-        self.game_over_flags: list[CombatView] = [None] * size
+        self._state_ts: list[CombatView] = [None] * size
+        self._state_tp1s: list[CombatView] = [None] * size
+        self._actions: list[int] = [None] * size
+        self._rewards: list[float] = [None] * size
+        self._valid_action_mask_tp1s: list[CombatView] = [None] * size
+        self._game_over_flags: list[CombatView] = [None] * size
+
+        # Current write index
+        self._index: int = 0
+
+        # Whether the buffer is full or not
+        self._full: bool = False
+
+        # RNG for sampling TODO: seed
+        self._rng = np.random.default_rng()
+
+    @property
+    def full(self) -> bool:
+        return self._full
 
     def __len__(self) -> int:
-        return self.size
+        return self._size
 
+    def store(self, sample: Sample) -> None:
+        self._state_ts[self._index] = sample.state_t
+        self._state_tp1s[self._index] = sample.state_tp1
+        self._actions[self._index] = sample.action
+        self._rewards[self._index] = sample.reward
+        self._valid_action_mask_tp1s[self._index] = sample.valid_action_mask_tp1
+        self._game_over_flags[self._index] = sample.game_over_flag
 
-def store(replay_buffer: ReplayBuffer, sample: Sample, index: int) -> int:
-    replay_buffer.state_ts[index] = sample.state_t
-    replay_buffer.state_tp1s[index] = sample.state_tp1
-    replay_buffer.actions[index] = sample.action
-    replay_buffer.rewards[index] = sample.reward
-    replay_buffer.valid_action_mask_tp1s[index] = sample.valid_action_mask_tp1
-    replay_buffer.game_over_flags[index] = sample.game_over_flag
+        # Increment index
+        self._index = (self._index + 1) % len(self)
 
-    return (index + 1) % len(replay_buffer)
+        # Set full if needed
+        self._full = self._full or self._index == 0
 
+    def sample(self, batch_size: int) -> Optional[Batch]:
+        if self._index < batch_size and not self._full:
+            return
 
-def sample(replay_buffer: ReplayBuffer, batch_size: int, up_to: int) -> Optional[Batch]:
+        high = len(self) if self._full else self._index
+        sample_indexes = self._rng.integers(low=0, high=high, size=batch_size)
 
-    # TODO: should happen outside of this function
-    # if not full and ptr < batch_size:
-    #     return None
+        # Initialize empty batch
+        batch = Batch(
+            state_ts=[None] * batch_size,
+            state_tp1s=[None] * batch_size,
+            actions=[None] * batch_size,
+            rewards=[None] * batch_size,
+            valid_action_mask_tp1s=[None] * batch_size,
+            game_over_flags=[None] * batch_size,
+        )
 
-    rng = np.random.default_rng()  # TODO: seed
-    sample_indexes = rng.integers(low=0, high=up_to, size=batch_size)
+        # Fill it
+        for batch_index, sample_index in enumerate(sample_indexes):
+            batch.state_ts[batch_index] = self._state_ts[sample_index]
+            batch.state_tp1s[batch_index] = self._state_tp1s[sample_index]
+            batch.actions[batch_index] = self._actions[sample_index]
+            batch.rewards[batch_index] = self._rewards[sample_index]
+            batch.valid_action_mask_tp1s[batch_index] = self._valid_action_mask_tp1s[sample_index]
+            batch.game_over_flags[batch_index] = self._game_over_flags[sample_index]
 
-    # Initialize empty batch
-    batch = Batch(
-        state_ts=[None] * batch_size,
-        state_tp1s=[None] * batch_size,
-        actions=[None] * batch_size,
-        rewards=[None] * batch_size,
-        valid_action_mask_tp1s=[None] * batch_size,
-        game_over_flags=[None] * batch_size,
-    )
-
-    # Fill it
-    for batch_index, sample_index in enumerate(sample_indexes):
-        batch.state_ts[batch_index] = replay_buffer.state_ts[sample_index]
-        batch.state_tp1s[batch_index] = replay_buffer.state_tp1s[sample_index]
-        batch.actions[batch_index] = replay_buffer.actions[sample_index]
-        batch.rewards[batch_index] = replay_buffer.rewards[sample_index]
-        batch.valid_action_mask_tp1s[batch_index] = replay_buffer.valid_action_mask_tp1s[
-            sample_index
-        ]
-        batch.game_over_flags[batch_index] = replay_buffer.game_over_flags[sample_index]
-
-    return batch
+        return batch
