8800c68698686d7db8ce4abbcc4b530c68edd40d
Guillermo Etchebarne <guillermo.etchebarne@gmail.com>
2024-07-10 17:42:09 -0300

    Improve state changes separation of concerns

--- src/game/combat/test.py
+++ src/game/combat/test.py
@@ -2,7 +2,6 @@ from src.agents.random import RandomAgent
 from src.game.combat.create import create_combat
 from src.game.combat.drawer import drawer
 from src.game.combat.effect_queue import EffectQueue
-from src.game.combat.effect_queue import process_queue
 from src.game.combat.entities import Entities
 from src.game.combat.handle_input import handle_action
 from src.game.combat.phase import combat_start
@@ -31,23 +30,13 @@ def main():
     while not is_game_over(entities):
         #
         combat_view = view_combat(entities, effect_queue, state)
-        print(state)
         drawer(combat_view)
 
         # Get action form agent
         action = agent.select_action(combat_view)
-        print(action)
 
         # Handle action
         state = handle_action(entities, effect_queue, state, action)
-
-        # Process effect queue
-        process_queue(entities, effect_queue)
-
-        if effect_queue.get_pending() is None:
-            continue
-
-        state = State.AWAIT_EFFECT_TARGET
 
 
 if __name__ == "__main__":
