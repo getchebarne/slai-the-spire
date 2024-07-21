9d9a5540c9ff92d0cca1f9445841634925d1b363
Guillermo Etchebarne <guillermo.etchebarne@gmail.com>
2024-07-21 20:23:27 -0300

    Improve main game logic

--- src/game/combat/create.py
+++ src/game/combat/create.py
@@ -1,3 +1,4 @@
+from src.game.combat.effect_queue import EffectQueue
 from src.game.combat.entities import Entities
 from src.game.combat.factories import defend
 from src.game.combat.factories import dummy
@@ -6,14 +7,14 @@ from src.game.combat.factories import neutralize
 from src.game.combat.factories import silent
 from src.game.combat.factories import strike
 from src.game.combat.factories import survivor
+from src.game.combat.manager import CombatManager
+from src.game.combat.phase import combat_start
 
 
-def create_combat() -> Entities:
-    # Create Entities instance
-    entities = Entities()
-
-    # Fill
+def create_combat_manager() -> CombatManager:
+    # Create entities
     # TODO: create functions for this
+    entities = Entities()
     entities.character_id = entities.create_entity(silent())
     entities.monster_ids = [entities.create_entity(dummy())]
     entities.energy_id = entities.create_entity(energy())
@@ -31,5 +32,14 @@ def create_combat() -> Entities:
         entities.create_entity(neutralize()),
         entities.create_entity(survivor()),
     }
+    # Create effect queue
+    effect_queue = EffectQueue()
 
-    return entities
+    # Create combat manager w/ entities and effect queue
+    combat_manager = CombatManager(entities=entities, effect_queue=effect_queue)
+
+    # Queue combat start effects
+    # TODO: rename this function
+    combat_start(combat_manager)
+
+    return combat_manager
