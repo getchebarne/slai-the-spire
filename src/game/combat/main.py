c94af9ab3a71e19e1b25cdeb7095eb190c09905f
Guillermo Etchebarne <guillermo.etchebarne@gmail.com>
2024-07-21 20:23:27 -0300

    Improve main game logic

--- src/game/combat/main.py
+++ src/game/combat/main.py
@@ -1,9 +1,9 @@
 from typing import Optional
 
 from src.agents.random import RandomAgent
-from src.game.combat.create import create_combat
+from src.agents.random import BaseAgent
+from src.game.combat.create import create_combat_manager
 from src.game.combat.drawer import drawer
-from src.game.combat.effect_queue import EffectQueue
 from src.game.combat.effect_queue import process_queue
 from src.game.combat.entities import Card
 from src.game.combat.entities import Effect
@@ -11,19 +11,10 @@ from src.game.combat.entities import EffectTargetType
 from src.game.combat.entities import EffectType
 from src.game.combat.handle_input import handle_action
 from src.game.combat.manager import CombatManager
-from src.game.combat.phase import combat_start
 from src.game.combat.state import State
 from src.game.combat.state import on_enter
 from src.game.combat.utils import is_game_over
 from src.game.combat.view import view_combat
-
-
-# Instantiate combat manager
-combat_manager = CombatManager(entities=create_combat(), effect_queue=EffectQueue())
-combat_start(combat_manager)
-
-# Instance agent
-agent = RandomAgent()
 
 
 def _card_requires_target(card: Card) -> bool:
@@ -41,7 +32,7 @@ def _set_state(
     combat_manager.state = state
 
 
-def _aux(combat_manager: CombatManager) -> None:
+def _process_round(combat_manager: CombatManager) -> None:
     card_active_id = combat_manager.entities.card_active_id
     if card_active_id is not None and card_active_id in combat_manager.entities.card_in_hand_ids:
         # Check if the card requires target
@@ -68,10 +59,10 @@ def _aux(combat_manager: CombatManager) -> None:
     _set_state(State.AWAIT_EFFECT_TARGET, combat_manager, query_ids)
 
 
-def main():
-
+def main(combat_manager: CombatManager, agent: BaseAgent) -> None:
     while not is_game_over(combat_manager.entities):
-        _aux(combat_manager)
+        # Process round
+        _process_round(combat_manager)
 
         # Get combat view and draw it on the terminal
         combat_view = view_combat(combat_manager)
@@ -85,4 +76,11 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # Instance combat manager
+    combat_manager = create_combat_manager()
+
+    # Instance agent
+    agent = RandomAgent()
+
+    # Execute
+    main(combat_manager, agent)
