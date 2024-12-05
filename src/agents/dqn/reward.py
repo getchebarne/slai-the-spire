f22f3fe02904e007e169161477c64c14a38877f0
Guillermo Etchebarne <guillermo.etchebarne@gmail.com>
2024-12-05 13:51:47 -0300

    Decrease reward delay and add periodic model saving

--- src/agents/dqn/reward.py
+++ src/agents/dqn/reward.py
@@ -1,14 +1,8 @@
 from src.game.combat.view import CombatView
 
 
-def compute_reward(combat_view: CombatView, game_over_flag: bool) -> float:
-    if not game_over_flag:
-        # Still going
-        return 0
-
-    if combat_view.character.health.current <= 0:
-        # Loss
-        return -1
-
-    # Win TODO: the agent may gain health, think about this
-    return combat_view.character.health.current / combat_view.character.health.max
+def compute_reward(combat_view_t: CombatView, combat_view_tp1: CombatView) -> float:
+    # Still going
+    return (
+        combat_view_tp1.character.health.current - combat_view_t.character.health.current
+    ) / combat_view_t.character.health.max
