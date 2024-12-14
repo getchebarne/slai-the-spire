14cd76aca03f96471e91f787e73628a09ca37448
Guillermo Etchebarne <guillermo.etchebarne@gmail.com>
2024-12-14 13:51:48 -0300

    Add combat loss penalization in reward

--- src/agents/dqn/reward.py
+++ src/agents/dqn/reward.py
@@ -5,6 +5,11 @@ def compute_reward(
     combat_view_t: CombatView, combat_view_tp1: CombatView, game_over_flag: bool
 ) -> float:
     if game_over_flag:
+        if combat_view_tp1.character.health.current <= 0:
+            # Loss
+            return -1
+
+        # Win
         return combat_view_tp1.character.health.current / combat_view_tp1.character.health.max
 
     # Still going
