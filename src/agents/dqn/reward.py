a659202fc43f7f1c9cd0069ed116b82d4681e845
Guillermo Etchebarne <guillermo.etchebarne@gmail.com>
2024-12-22 14:30:47 -0300

    Update reward function

--- src/agents/dqn/reward.py
+++ src/agents/dqn/reward.py
@@ -12,7 +12,26 @@ def compute_reward(
         # Win
         return combat_view_tp1.character.health.current / combat_view_tp1.character.health.max
 
-    # Still going
-    return (
+    # Card diff
+    weight_cards_in_hand = 0.0075
+    diff_cards_in_hand = weight_cards_in_hand * (
+        len(combat_view_tp1.hand) - len(combat_view_t.hand)
+    )
+
+    # Energy diff
+    weight_energy = 0.0075
+    diff_energy = weight_energy * (combat_view_tp1.energy.current - combat_view_t.energy.current)
+
+    # Damage
+    # weight_damage = 0.0375
+    # diff_damage = weight_damage * (
+    #     combat_view_tp1.monsters[0].health.current - combat_view_t.monsters[0].health.current
+    # )
+
+    # Health
+    diff_health = (
         combat_view_tp1.character.health.current - combat_view_t.character.health.current
-    ) / combat_view_tp1.character.health.max
+    ) / combat_view_t.character.health.max
+
+    # Still going
+    return diff_health + diff_cards_in_hand + diff_energy
