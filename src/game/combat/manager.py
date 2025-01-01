bfb33f1f5542ea295cbcdc74bcec0cfdc8118024
Guillermo Etchebarne <guillermo.etchebarne@gmail.com>
2025-01-01 19:45:37 -0300

    Update state change logic

--- src/game/combat/manager.py
+++ src/game/combat/manager.py
@@ -4,6 +4,7 @@ from typing import Optional
 from src.game.combat.effect_queue import EffectQueue
 from src.game.combat.entities import Entities
 from src.game.combat.state import State
+from src.game.combat.state import on_enter
 
 
 @dataclass
@@ -11,3 +12,11 @@ class CombatManager:
     entities: Entities
     effect_queue: EffectQueue
     state: Optional[State] = None
+
+
+def change_state(
+    combat_manager: CombatManager, state: State, entity_selectable_ids: Optional[list[int]] = None
+) -> None:
+    combat_manager.state = state
+
+    on_enter(combat_manager.entities, state, entity_selectable_ids)
